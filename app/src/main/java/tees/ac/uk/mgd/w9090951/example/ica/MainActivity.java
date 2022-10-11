add0e10fd79f4469c042b4bfd4037b209740ffc0
DALLALI <w9090951@tu54259.windows.tees.ac.uk>
2022-10-11 13:22:44 +0100

    Initial commit

---
+++ app/src/main/java/tees/ac/uk/mgd/w9090951/example/ica/MainActivity.java
@@ -0,0 +1,14 @@
+package tees.ac.uk.mgd.w9090951.example.ica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
